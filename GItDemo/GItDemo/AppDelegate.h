63dd83aeb3bb16c56ca93cfa1f0926a817cd17cc
xiaodaokuailai <chenglibin@chenglibindeMacBook-Pro.local>
2016-08-01 10:41:44 +0800

    创建测试工程
    测试工程

---
+++ GItDemo/GItDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GItDemo
+//
+//  Created by chenglibin on 16/8/1.
+//  Copyright © 2016年 chenglibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
